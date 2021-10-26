de1f75af1f51ad18c59b8a161350a75226e1a7af
sergeyfilip <sfilperz@gmail.com>
2021-10-26 11:32:53 +0300

    some error check added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
+use fstat::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,25 +13,12 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     
-    let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file");
-    
-    println!("With text:\n{}", contents);
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    if let Err(e) = fstat::run(config) {
+        println!("Application error: {}", e);
         
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        
-        Ok(Config { query, filename })
+        process::exit(1);
     }
+    
 }
+
 
