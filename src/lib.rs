da3560092f0a037812380da3f4a293fa8d6b5ff9
sergeyfilip <sfilperz@gmail.com>
2021-10-27 09:28:50 +0300

    some statistic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::collections::HashMap;
 
 pub struct Config {
     pub query: String,
@@ -20,7 +21,50 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     
-    println!("With text:\n{}", contents);
-    
+   // println!("With text:\n{}", contents);
+    stat(&contents);
     Ok(())
+}
+pub fn stat (contents: &str) {
+    
+    let mut vect:Vec<&str> = Vec::new();
+    let mut map = HashMap::new();
+    
+    for line in contents.lines() {
+        
+        let split = line.split_whitespace();
+        for s in split {
+            let mut m = s;
+            if value_have_bad_end(s) == true { m = rem_last(s); }
+          //  println!("{}  - {}  -    {}", m , m.len(),s.chars().last().unwrap())
+          vect.push(m);
+        }
+        
+    }
+    for word in vect {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    
+    println!("{:?}", map);
+    
+    
+    
+}
+pub fn rem_last(value: &str) -> &str {
+    let mut chars = value.chars();
+   // chars.next();
+    chars.next_back();
+    chars.as_str()
+}
+pub fn value_have_bad_end(value: &str) ->bool {
+    let s = value.chars().last().unwrap();
+    let mut z: bool = false;
+    if s == ',' { z = true; }
+    if s == '.' { z = true; }
+    if s == '?' { z = true; }
+    if s == '!' { z = true; }
+    if s == ';' { z = true; }
+    z
+    
 }
